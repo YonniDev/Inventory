493eadc538149f54511fe04f08454c41fc3fe4c7
YonniDev <119079610+YonniDev@users.noreply.github.com>
2026-04-23 23:29:28 -0400

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,41 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author "Pappu"
+description 'qb-inventorynp'
+version '1.0.4'
+
+shared_scripts {
+	'config.lua',
+	'@qb-weapons/config.lua'
+}
+
+lua54 'yes'
+
+server_scripts {
+	'@oxmysql/lib/MySQL.lua',
+	'server/main.lua',
+	-- 'server/visual.lua',
+}
+
+client_scripts {
+	'client/main.lua',
+}
+
+
+ui_page {
+	'nui/pappunp.html'
+}
+
+files {
+	'nui/pappunp.html',
+	'nui/css/main.css',
+	'nui/js/app.js',
+	'nui/images/*.svg',
+	'nui/images/*.png',
+	'nui/images/*.jpg',
+	'nui/inventory_images/*.png',
+	'nui/ammo_images/*.png',
+	'nui/attachment_images/*.png',
+	'nui/*.ttf'
+}
